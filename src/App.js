bf8e5ad28fced4c608f19a1a87a4443ef7174a10
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2021-10-20 14:51:19 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import Navbar from './components/NavBar/Navbar';
 
 function App() {
   return (
     <div className="background">
       <Router>
-          <Navbar />
           <main>
-            <h1>Alfie Ranstead</h1>
-            <h2>Programmer and Student</h2>
+            <h1>MonoPage</h1>
+            <h2>An Entertainment Platform for the future</h2>
           </main>
       </Router>
     </div>
