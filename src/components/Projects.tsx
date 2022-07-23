e90a8e04e5eea038a51fd447292fae89e68c13f6
AlfieRan <alfie.ranstead@outlook.com>
2022-07-23 14:41:51 +0200

    updated paper on site

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => (
       </Text>
       <Text maxW={"800px"} mb={3}>
         I&apos;ve done so many different things it&apos;s hard to summarise them
-        all, but here&apos;s the top two projects I&apos;m working on right now!
+        all, but here&apos;s the top things I&apos;m working on right now!
       </Text>
       <Center flexWrap={"wrap"}>
         <SingleProject
@@ -25,6 +25,12 @@ const Projects = () => (
           info={"A more Efficent, more Eco-friendly, custom built Blockchain."}
           image={"/Projects/Blockchain.jpeg"}
           link={"https://monochain.network"}
+        />
+        <SingleProject
+          title={"Computational Capacity"}
+          info={"A Paper on the Computational Capacity of Spherical Systems."}
+          image={"/Projects/ComputingCapacity.png"}
+          link={"/papers/ComputationalCapacity.pdf"}
         />
       </Center>
     </Center>
