a5b58a6230cd6ac56151396029e0240b14378fc3
AlfieRan <alfie.ranstead@outlook.com>
2022-07-29 23:06:23 +0200

    commenting

--- src/pages/api/aws/info.ts
+++ src/pages/api/aws/info.ts
@@ -1,11 +1,19 @@
 import aws from "aws-sdk";
 
+// Config Constants Declared here
+let loaded = false;
+const fileExtensions = ["png", "jpg", "jpeg", "gif"];
+// End of Config Constants - Testing Data at end of file
+
+// AWS variables loaded from environment
 const awsPoolId = process.env.AWS_POOL_ID;
 const bucketName = process.env.AWS_BUCKET_NAME;
 const bucketUrl = `https://${bucketName}.s3.amazonaws.com/`;
 let s3: aws.S3;
-let loaded = false;
+const testing = process.env.DEVELOPMENT === "true";
+// end environment variables
 
+// build s3 connection if variables are valid
 if (awsPoolId === undefined || bucketName === undefined) {
     console.log("AWS_POOL_ID or AWS_BUCKET_NAME is not defined");
     console.log("AWS_POOL_ID: " + awsPoolId);
@@ -20,46 +28,53 @@ if (awsPoolId === undefined || bucketName === undefined) {
     });
     loaded = true;
 }
+// end of building s3 connection
 
+// start of api route
 export default async function awsS3Bucket(req: any, res: any) {
     if (loaded && bucketName !== undefined) {
-        s3.listObjects(
-            { Prefix: "SummerMaths", Bucket: bucketName },
-            (err: any, data: any) => {
-                if (err) {
-                    console.log(
-                        err,
-                        "\n\nErrored using poolId and BucketId of:",
-                        awsPoolId,
-                        bucketName
-                    );
-                    res.status(500).send(err);
-                } else {
-                    const files = parseFiles(data);
-                    const obj = parseObj(files);
-                    console.log(obj);
-                    res.status(200).send(obj);
-
-                    // res.status(200).send(
-                    //     `<html><body>${files.map(
-                    //         (file: any) =>
-                    //             "<img src=" +
-                    //             bucketUrl +
-                    //             file +
-                    //             " height=100px />"
-                    //     )}</body></html>`
-                    // );
+        if (testing) {
+            console.log(
+                "Using Test Data for request as per environment variable"
+            );
+            const obj = HandleResponse(testingData);
+            res.status(200).send(obj);
+        } else {
+            s3.listObjects(
+                {
+                    Prefix: "SummerMaths",
+                    Bucket: bucketName,
+                },
+                (err: any, data: aws.S3.ListObjectsOutput) => {
+                    if (err) {
+                        console.log(
+                            err,
+                            "\n\nErrored using poolId and BucketId of:",
+                            awsPoolId,
+                            bucketName
+                        );
+                        res.status(500).send(err);
+                    } else {
+                        const obj = HandleResponse(data);
+                        res.status(200).send(obj);
+                    }
                 }
-            }
-        );
+            );
+        }
     } else {
         res.status(500).json({
             error: "Something went wrong",
         });
     }
 }
+// end of api route
 
-const fileExtensions = ["png", "jpg", "jpeg", "gif"];
+// start of util functions
+function HandleResponse(input: aws.S3.ListObjectsOutput) {
+    const files = parseFiles(input);
+    const obj = parseObj(files);
+    return obj;
+}
 
 function parseFiles(obj: any) {
     const output: string[] = [];
@@ -121,3 +136,65 @@ function parseObj(data: string[]): objType {
 
     return output;
 }
+// end of util functions
+
+// start of testing data
+const testingData = {
+    IsTruncated: false,
+    Marker: "",
+    Contents: [
+        {
+            Key: "SummerMaths/Ch1/a20.png",
+            LastModified: new Date("2022-07-28T14:08:14.000Z"),
+            ETag: '"0222e383670bea09a81fb75cb510f9c8"',
+            ChecksumAlgorithm: [],
+            Size: 9719,
+            StorageClass: "STANDARD",
+        },
+        {
+            Key: "SummerMaths/Ch1/q20.png",
+            LastModified: new Date("2022-07-28T14:08:18.000Z"),
+            ETag: '"6589fd116c3bd6ea481a406604415549"',
+            ChecksumAlgorithm: [],
+            Size: 64645,
+            StorageClass: "STANDARD",
+        },
+        {
+            Key: "SummerMaths/random/",
+            LastModified: new Date("2022-07-29T11:08:29.000Z"),
+            ETag: '"d41d8cd98f00b204e9800998ecf8427e"',
+            ChecksumAlgorithm: [],
+            Size: 0,
+            StorageClass: "STANDARD",
+        },
+        {
+            Key: "SummerMaths/random/CrabGameUpdate.png",
+            LastModified: new Date("2022-07-29T11:10:08.000Z"),
+            ETag: '"dd934ad6e33d26076bc44eb2faf47ed2"',
+            ChecksumAlgorithm: [],
+            Size: 429685,
+            StorageClass: "STANDARD",
+        },
+        {
+            Key: "SummerMaths/random/bear.png",
+            LastModified: new Date("2022-07-29T17:20:39.000Z"),
+            ETag: '"6ec2293b5fc89434c0d262c79d0695a6"',
+            ChecksumAlgorithm: [],
+            Size: 1529249,
+            StorageClass: "STANDARD",
+        },
+        {
+            Key: "SummerMaths/random/image.png",
+            LastModified: new Date("2022-07-29T11:27:42.000Z"),
+            ETag: '"7bb3c7aee0fad913c7a6432e2a6cc6f0"',
+            ChecksumAlgorithm: [],
+            Size: 1793271,
+            StorageClass: "STANDARD",
+        },
+    ],
+    Name: "maths-questions-bucket",
+    Prefix: "SummerMaths",
+    MaxKeys: 1000,
+    CommonPrefixes: [],
+};
+// end of testing data
