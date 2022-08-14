8ea7391c4adab141c1ebe4be43b5c2c6f0dc9a14
AlfieRan <alfie.ranstead@outlook.com>
2022-08-14 17:00:46 +0100

    added localstorage info

--- src/utils/hooks/useMode.ts
+++ src/utils/hooks/useMode.ts
@@ -3,13 +3,25 @@ import useHeight from "./window/useHeight";
 import { useEffect, useState } from "react";
 import useWidth from "./window/useWidth";
 
+const storageKey = "mode";
+
+type modes = "mobile" | "desktop" | undefined;
+
 export default function useMode() {
+    let localStorage: modes = undefined;
+    try {
+        const raw = window.localStorage.getItem(storageKey);
+        if (raw == "mobile" || raw == "desktop") {
+            localStorage = raw;
+        }
+    } catch (e) {
+        console.log("Error: localStorage is not available.", e);
+    }
+
     const aspect = useAspect();
     const height = useHeight();
     const width = useWidth();
-    const [mode, setMode] = useState<"mobile" | "desktop" | undefined>(
-        undefined
-    );
+    const [mode, setMode] = useState<modes>(localStorage);
 
     useEffect(() => {
         if (
@@ -26,6 +38,16 @@ export default function useMode() {
             console.log("Setting mode:", mode);
         }
     }, [aspect, height]);
+
+    useEffect(() => {
+        try {
+            if (mode !== undefined) {
+                window.localStorage.setItem(storageKey, mode);
+            }
+        } catch (e) {
+            console.log("Error: localStorage is not available.", e);
+        }
+    }, [mode]);
 
     return mode;
 }
