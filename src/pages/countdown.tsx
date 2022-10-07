fb6fc85c26ae43e7f3b04122e4fd8a6e1d966167
AlfieRan <alfie.ranstead@outlook.com>
2022-10-07 12:09:01 +0100

    feat: Countdown page now kills chrome with 1000000 audio files

--- src/pages/countdown.tsx
+++ src/pages/countdown.tsx
@@ -1,10 +1,15 @@
-import { Box, Center, Flex, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Center, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import sleep from "../utils/time/sleep";
+
+const files = ["/countdown/laughing.mp3", "/countdown/bell.mp3"];
 
 export default function Page() {
     const theEnd = 1665136038803;
     const msInDay = 1000 * 60 * 60 * 24;
+    const initialTimeBetween = 2500;
+    const [running, setRunning] = useState(true);
 
     function getDaysUntilEnd() {
         return (
@@ -13,11 +18,51 @@ export default function Page() {
         );
     }
 
+    function newAudio() {
+        try {
+            const audio = new Audio(
+                files[Math.floor(Math.random() * files.length)]
+            );
+            sleep(1000).then(() =>
+                audio
+                    .play()
+                    .then((res) => {
+                        console.log("Audio started");
+                    })
+                    .catch(console.error)
+            );
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     const [daysLeft, setDaysLeft] = useState(getDaysUntilEnd());
 
     setInterval(() => {
         setDaysLeft(getDaysUntilEnd());
     }, 100);
+
+    useEffect(() => {
+        newAudio();
+        (async () => {
+            let timeBetween = initialTimeBetween;
+            while (running) {
+                await sleep(timeBetween);
+                newAudio();
+                timeBetween = timeBetween * 0.9;
+                console.log(
+                    "time between audio:",
+                    timeBetween,
+                    "should next be",
+                    timeBetween * 0.9
+                );
+
+                if (timeBetween < 10) {
+                    setRunning(false);
+                }
+            }
+        })();
+    }, []);
 
     return (
         <Center w={"100vw"} h={"100vh"} p={"50px"}>
@@ -29,14 +74,16 @@ export default function Page() {
                         position={"absolute"}
                         w={"100%"}
                         h={"100%"}
-                        opacity={"25%"}
+                        opacity={"100%"}
                         zIndex={0}
                     >
                         <Image src={"/countdown/bg.jpg"} layout={"fill"} />
                     </Box>
-                    <Center zIndex={1}>
-                        <Text fontSize={"5xl"}>Time&apos;s up 😈</Text>
-                    </Center>
+                    <Box position={"absolute"} bottom={5} zIndex={1}>
+                        <Text fontSize={"min(200px, 15vw)"}>
+                            Time&apos;s up 😈
+                        </Text>
+                    </Box>
                 </Center>
             )}
         </Center>
