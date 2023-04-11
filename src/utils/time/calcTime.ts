4eecc6abd9428b3e5ba3426937a436fff6ddd9ad
AlfieRan <alfie.ranstead@outlook.com>
2023-04-11 23:56:10 +0100

    patch: updated time calculation

--- src/utils/time/calcTime.ts
+++ src/utils/time/calcTime.ts
@@ -1,22 +1,25 @@
-export default function calcDif(
-  year: number,
-  month: number,
-  date: number
-): string {
-  const dif = Date.now() - new Date().setFullYear(year, month, date);
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = 60 * SECONDS_PER_MINUTE;
+const SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR;
+const SECONDS_PER_YEAR =
+    365 * SECONDS_PER_DAY + 6 * SECONDS_PER_HOUR + 9 * SECONDS_PER_MINUTE + 9;
 
-  return make4Digit(
-    (Math.round(dif / (60 * 60 * 24 * 365.25)) / 1000).toString()
-  );
+export default function calcDif(
+    year: number,
+    month: number,
+    date: number
+): string {
+    const milliseconds =
+        new Date().getTime() - new Date().setFullYear(year, month - 1, date);
+
+    const years = milliseconds / (SECONDS_PER_YEAR * 1000);
+
+    return threeDecimals(years);
 }
 
-function make4Digit(num: string): string {
-  const integers = num.split(".")[0];
-  let decimals = num.split(".")[1] ?? "0";
-
-  while (decimals.length < 3) {
-    decimals += "0";
-  }
-
-  return integers + "." + decimals;
+function threeDecimals(inp: number): string {
+    const splitInput = inp.toString().split(".");
+    const integer = splitInput[0];
+    const decimal = splitInput[1].slice(0, 3);
+    return `${integer}.${decimal}`;
 }
